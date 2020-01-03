07075f2ade1fcd6744c0d592aa05a614d41d32f2
TIEC <TIEC@infos-iMac.local>
2020-01-03 14:06:37 -0500

    styled homepage

--- client/src/containers/HomePage/HomePage.js
+++ client/src/containers/HomePage/HomePage.js
@@ -4,8 +4,6 @@ import Aux from '../../hoc/Aux/Aux';
 import Button from '../../components/UI/Button/Button';
 import Pages from '../../hoc/Pages/Pages';
 import 'katex/dist/katex.min.css';
-// import { BlockMath} from 'react-katex';
-// InlineMath
 import classes from './HomePage.module.scss';
 
 class HomePage extends Component{
@@ -19,31 +17,36 @@ class HomePage extends Component{
 
           <div className={classes['row']}>
             <section className={classes['home']}>
+              <div className={classes['home__title']}>
+                <h1 className={classes['home__title--heading']}>
+                Hi, I'm <span className={classes['home__title--span']}>Chufan Wu.</span>
+                </h1>
+              </div>
 
+              <div className={classes['home__message']}>
 
-              <div className={classes['home__main']}>
-                <p className={classes['home__main--paragraph']}>
-                  Hi, I'm Chufan. I'm currently away from home. I had imformed Schrodinger to take care of any incidants.
+                <p className={classes['home__message--paragraph']}>
+
+                  I'm a physics student graduated from University of Toronto.
+                </p>
+                <p className={classes['home__message--paragraph']}>
+                  I'm currently away from home. I had imformed Schrodinger to take care of any incidants.
                   Please do make yourself comfortable, roam around, I have put my work in here.
                 </p>
-                <p className={classes['home__main--paragraph']}>
+                <p className={classes['home__message--paragraph']}>
                   If you want to learn more about me, use this portal :)
                 </p>
-
+                <div className={classes['home__message--button']}>
                 <NavLink to='/about'>
                   <Button type='text-2'>
                     About me &rarr;
                   </Button>
                 </NavLink>
-
-
-                {/* <div className={classes['heading-secondary']}>
-                  <BlockMath math="\nabla \cdot \vec{E}=\frac \rho {\epsilon_0}"/>
-                </div> */}
+                </div>
               </div>
               <div className={classes['home__announcement']}>
-                <h3 className={classes['heading-secondary']}>Announcement:</h3>
-                <p>
+                <h3 className={classes['home__announcememt--heading']}>Announcement:</h3>
+                <p className={classes['home__announcement--content']}>
                   The Meowster is currently working hard on expanding this universe.
                 </p>
               </div>
