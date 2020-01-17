f46d1053d1c9d01dc268a54adc6236b33f9b6fe4
Jeffrey Wu <wuchufan@Jeffreys-MacBook-Pro.local>
2020-01-17 17:59:15 -0500

    micro adjustment on about page

--- client/src/containers/About/AboutHeader/AboutHeader.js
+++ client/src/containers/About/AboutHeader/AboutHeader.js
@@ -7,10 +7,13 @@ const aboutHeader = (props) => {
 
   return(
     <header className={classes['header']}>
+      
       <div className={classes['text-box']}>
         <h1 className={classes['text-box__text']}>Hello, I'm Chufan Wu</h1>
         <h1 className={classes['text-box__text']}>I play video games.</h1>
         <h1 className={classes['text-box__text']}>I love knowledge.</h1>
+        <h1 className={classes['text-box__text']}>I love knowledge.</h1>
+
       </div>
       <div className={classes['intro']}>
 
