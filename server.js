74d8e01bcb4998325ecf526d163afcf55f8357c9
wuchufan <49216918+wuchufan@users.noreply.github.com>
2020-02-05 20:59:14 -0500

    fixed typo on server.js, see if it solve the redirect problem

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get('/posts/:id',(req,res)=>{
 if (process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
   app.get('*',(req,res)=>{
-    res.senFile(path.resolve(__dirname,'client','build','index.html'))
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
   })
 }
 
