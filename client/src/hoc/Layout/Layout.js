7c6bbf6d506e5564470784235323eb2f1ad6ad0c
wuchufan <49216918+wuchufan@users.noreply.github.com>
2020-03-15 04:01:42 -0400

    tril

--- client/src/hoc/Layout/Layout.js
+++ client/src/hoc/Layout/Layout.js
@@ -17,14 +17,13 @@ import Background from '../../components/Background/Background';
 
 
 class Layout extends Component {
-
   render() {
-
+    const vh = window.innerHeight * 0.01;
     return (<React.Fragment>
       <div className={classes['background']}>
       <Background/>
       </div>
-      <main className={classes['main']}>
+      <main className={classes['main']} style={{minHeight:100*vh}}>
 
         <Route render={({location}) => {
           return (<React.Fragment>
